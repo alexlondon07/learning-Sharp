3f83c78fc46252aac3cf7933bd446e1a5e051063
Alexander Londoño Espejo <alexander.londono@secuencia24.com>
2016-11-15 17:33:30 -0500

    Tipos de datos

---
+++ learnCsharpCodigoFacilito/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace learnCsharpCodigoFacilito
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World! Learn C# nuevamente");
+			Console.ReadKey ();
+
+			int i = 0;
+			decimal x = 0.0m;
+			float f = 0.0f;
+			double d = 0.0D;
+			string cadena = "Hola Mundo c#";
+			bool sw = false;
+		}
+	}
+}
